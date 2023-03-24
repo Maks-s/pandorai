96bc4c24ff6f9e07193d48a032491149027d68bc
Maks-s <maks.service@protonmail.com>
2023-03-24 18:49:57 +0100

    ✨ Add conversation title search

--- components/ChatSidebar.vue
+++ components/ChatSidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="h-full flex flex-col p-4">
-    <div class="flex-grow pb-4">
+    <n-space vertical :size="20" class="flex-grow pb-4">
       <n-button
         block
         type="primary"
@@ -12,23 +12,25 @@
         <span class="pl-2 pb-[2px]">{{ $t('NEW_CHAT') }}</span>
       </n-button>
 
-      <h4 class="text-center mt-6 text-lg font-bold">
-        {{ $t('CHAT_HISTORY') }}
-      </h4>
+      <n-input
+        type="text"
+        :placeholder="$t('SEARCH')"
+        @input="doSearch"
+      ></n-input>
 
-      <div>
+      <n-space vertical>
         <n-button
-          v-for="chatSession in chatHistoryStore.history"
+          v-for="chatSession in history"
           :key="chatSession.title"
           block
           ghost
-          class="!justify-start !my-3"
+          class="!justify-start"
           @click="setCurrentChat(chatSession)"
         >
           <span>{{ chatSession.title }}</span>
         </n-button>
-      </div>
-    </div>
+      </n-space>
+    </n-space>
 
     <SettingsModal />
   </nav>
@@ -40,6 +42,19 @@ import { useChatHistoryStore } from '~~/stores/chat-history';
 
 const chatHistoryStore = useChatHistoryStore();
 const chatStore = useChatStore();
+const history = ref(chatHistoryStore.history);
+
+function doSearch(search: string) {
+  if (search) {
+    const lowSearch = search;
+    history.value = chatHistoryStore.history.filter((v) =>
+      v.title.toLowerCase().includes(lowSearch)
+    );
+    return;
+  }
+
+  history.value = chatHistoryStore.history;
+}
 
 function setCurrentChat(chatSession: ChatSession) {
   chatStore.chatSession = chatSession;
